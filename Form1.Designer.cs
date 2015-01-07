39f2a5939e59a76cc7b152e368c65dbb29523d58
dvirarad <dvirarad@gmail.com>
2015-01-07 15:28:26 +0200

    Tip Caclulatur
    c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace CalculatorTip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbResturanName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb5Star = new System.Windows.Forms.RadioButton();
+            this.rb4Star = new System.Windows.Forms.RadioButton();
+            this.rb3Star = new System.Windows.Forms.RadioButton();
+            this.cbClecn = new System.Windows.Forms.CheckBox();
+            this.cbService = new System.Windows.Forms.CheckBox();
+            this.cbAtmo = new System.Windows.Forms.CheckBox();
+            this.cbLike = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbBill = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.tbTip = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTotalBill = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bSave = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resutrant Name";
+            // 
+            // tbResturanName
+            // 
+            this.tbResturanName.Location = new System.Drawing.Point(112, 12);
+            this.tbResturanName.Name = "tbResturanName";
+            this.tbResturanName.Size = new System.Drawing.Size(81, 20);
+            this.tbResturanName.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb5Star);
+            this.groupBox1.Controls.Add(this.rb4Star);
+            this.groupBox1.Controls.Add(this.rb3Star);
+            this.groupBox1.Location = new System.Drawing.Point(24, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(169, 56);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rate";
+            // 
+            // rb5Star
+            // 
+            this.rb5Star.AutoSize = true;
+            this.rb5Star.Location = new System.Drawing.Point(115, 33);
+            this.rb5Star.Name = "rb5Star";
+            this.rb5Star.Size = new System.Drawing.Size(53, 17);
+            this.rb5Star.TabIndex = 2;
+            this.rb5Star.TabStop = true;
+            this.rb5Star.Text = "5 Star";
+            this.rb5Star.UseVisualStyleBackColor = true;
+            // 
+            // rb4Star
+            // 
+            this.rb4Star.AutoSize = true;
+            this.rb4Star.Location = new System.Drawing.Point(62, 33);
+            this.rb4Star.Name = "rb4Star";
+            this.rb4Star.Size = new System.Drawing.Size(53, 17);
+            this.rb4Star.TabIndex = 1;
+            this.rb4Star.TabStop = true;
+            this.rb4Star.Text = "4 Star";
+            this.rb4Star.UseVisualStyleBackColor = true;
+            // 
+            // rb3Star
+            // 
+            this.rb3Star.AutoSize = true;
+            this.rb3Star.Location = new System.Drawing.Point(6, 33);
+            this.rb3Star.Name = "rb3Star";
+            this.rb3Star.Size = new System.Drawing.Size(53, 17);
+            this.rb3Star.TabIndex = 0;
+            this.rb3Star.TabStop = true;
+            this.rb3Star.Text = "3 Star";
+            this.rb3Star.UseVisualStyleBackColor = true;
+            // 
+            // cbClecn
+            // 
+            this.cbClecn.AutoSize = true;
+            this.cbClecn.Location = new System.Drawing.Point(25, 146);
+            this.cbClecn.Name = "cbClecn";
+            this.cbClecn.Size = new System.Drawing.Size(53, 17);
+            this.cbClecn.TabIndex = 4;
+            this.cbClecn.Text = "Clean";
+            this.cbClecn.UseVisualStyleBackColor = true;
+            // 
+            // cbService
+            // 
+            this.cbService.AutoSize = true;
+            this.cbService.Location = new System.Drawing.Point(141, 146);
+            this.cbService.Name = "cbService";
+            this.cbService.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cbService.Size = new System.Drawing.Size(62, 17);
+            this.cbService.TabIndex = 5;
+            this.cbService.Text = "Service";
+            this.cbService.UseVisualStyleBackColor = true;
+            // 
+            // cbAtmo
+            // 
+            this.cbAtmo.AutoSize = true;
+            this.cbAtmo.Location = new System.Drawing.Point(24, 169);
+            this.cbAtmo.Name = "cbAtmo";
+            this.cbAtmo.Size = new System.Drawing.Size(82, 17);
+            this.cbAtmo.TabIndex = 6;
+            this.cbAtmo.Text = "Atmosphere";
+            this.cbAtmo.UseVisualStyleBackColor = true;
+            // 
+            // cbLike
+            // 
+            this.cbLike.AutoSize = true;
+            this.cbLike.Location = new System.Drawing.Point(141, 169);
+            this.cbLike.Name = "cbLike";
+            this.cbLike.Size = new System.Drawing.Size(46, 17);
+            this.cbLike.TabIndex = 7;
+            this.cbLike.Text = "Like";
+            this.cbLike.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "bil";
+            // 
+            // tbBill
+            // 
+            this.tbBill.Location = new System.Drawing.Point(112, 35);
+            this.tbBill.Name = "tbBill";
+            this.tbBill.Size = new System.Drawing.Size(81, 20);
+            this.tbBill.TabIndex = 9;
+            this.tbBill.Text = "0.0";
+            this.tbBill.TextChanged += new System.EventHandler(this.tbBill_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(47, 192);
+            this.trackBar1.Maximum = 25;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // tbTip
+            // 
+            this.tbTip.Location = new System.Drawing.Point(145, 192);
+            this.tbTip.Name = "tbTip";
+            this.tbTip.Size = new System.Drawing.Size(30, 20);
+            this.tbTip.TabIndex = 11;
+            this.tbTip.Text = "0.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tip ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Total Bill";
+            // 
+            // tbTotalBill
+            // 
+            this.tbTotalBill.Location = new System.Drawing.Point(74, 233);
+            this.tbTotalBill.Name = "tbTotalBill";
+            this.tbTotalBill.Size = new System.Drawing.Size(65, 20);
+            this.tbTotalBill.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(178, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "%";
+            // 
+            // bSave
+            // 
+            this.bSave.Location = new System.Drawing.Point(145, 233);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(48, 19);
+            this.bSave.TabIndex = 16;
+            this.bSave.Text = "Save";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 290);
+            this.Controls.Add(this.bSave);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbTotalBill);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbTip);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.tbBill);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbLike);
+            this.Controls.Add(this.cbAtmo);
+            this.Controls.Add(this.cbService);
+            this.Controls.Add(this.cbClecn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbResturanName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbResturanName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb5Star;
+        private System.Windows.Forms.RadioButton rb4Star;
+        private System.Windows.Forms.RadioButton rb3Star;
+        private System.Windows.Forms.CheckBox cbClecn;
+        private System.Windows.Forms.CheckBox cbService;
+        private System.Windows.Forms.CheckBox cbAtmo;
+        private System.Windows.Forms.CheckBox cbLike;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbBill;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox tbTip;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTotalBill;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button bSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorTip
+{
+    public partial class Form1 : Form
+    {
+        private double tip = 10;
+        private double totalBill;
+        private double[] tipList = { 1, 2, 3, 1.5, 1.7, 1.3, 2.5 };
+        private bool[] flag = new bool[7];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //regester radio button event handler
+            rb3Star.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            rb4Star.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            rb5Star.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+
+            //regester CheckBox event Handler
+            cbClecn.CheckStateChanged += new EventHandler(checkBox_CheckedChanged);
+            cbService.CheckStateChanged += new EventHandler(checkBox_CheckedChanged);
+            cbLike.CheckStateChanged += new EventHandler(checkBox_CheckedChanged);
+            cbAtmo.CheckStateChanged += new EventHandler(checkBox_CheckedChanged);
+        }
+
+        private void tbBill_TextChanged(object sender, EventArgs e)
+        {
+            update();
+
+        }
+
+        private void update()
+        {
+            tip = 10;
+            for (int i = 0; i < flag.Length; i++)
+            {
+                if (flag[i])
+                {
+                    tip += tipList[i];
+                }
+            }
+            totalBill = Double.Parse(tbBill.Text);
+            totalBill += totalBill * tip / 100;
+            tbTotalBill.Text = "" + totalBill;
+            tbTip.Text = "" + tip;
+            trackBar1.Value = (int)tip;
+        }
+
+        private void radioButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (rb3Star.Checked)
+            {
+                flag[0] = true;
+                flag[1] = false;
+                flag[2] = false;
+            }
+            else if (rb4Star.Checked)
+            {
+                flag[0] = false;
+                flag[1] = true;
+                flag[2] = false;
+            }
+            else if (rb5Star.Checked)
+            {
+                flag[0] = false;
+                flag[1] = false;
+                flag[2] = true;
+            }
+            update();
+
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            int i = -1;
+            CheckBox cb = sender as CheckBox;
+            Boolean ff = cb.Checked;
+            if (cb == cbClecn) i = 3;
+            if (cb == cbAtmo) i = 4;
+            if (cb == cbService) i = 5;
+            if (cb == cbLike) i = 6;
+            flag[i] = ff;
+            update();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            tip = trackBar1.Value;
+            totalBill = Double.Parse(tbBill.Text);
+            totalBill += totalBill * tip / 100;
+            tbTotalBill.Text = "" + totalBill;
+            tbTip.Text = "" + tip;
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            // Displays a SaveFileDialog so the user can save the Image
+            // assigned to Button2.
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            saveFileDialog1.Title = "Save an Text File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+
+                System.IO.Stream fileStream = saveFileDialog1.OpenFile();
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(fileStream);
+                sw.WriteLine("Resturant Name: "+ tbResturanName.Text);
+                sw.WriteLine("Bill: " + tbBill.Text);
+                if (flag[0]) sw.WriteLine("Rate: 3 Star");
+                if (flag[1]) sw.WriteLine("Rate: 4 Star");
+                if (flag[2]) sw.WriteLine("Rate: 5 Star");
+                if (flag[3]) sw.WriteLine("Clean");
+                if (flag[4]) sw.WriteLine("Atmosphere");
+                if (flag[5]) sw.WriteLine("Service");
+                if (flag[6]) sw.WriteLine("Like");
+                sw.WriteLine("Tip: " + tbTip.Text+"%");
+                sw.WriteLine("Bill: " + tbTotalBill.Text);
+                sw.Flush();
+                sw.Close();
+
+            }
+        }
+
+
+    }
+
+
+}
